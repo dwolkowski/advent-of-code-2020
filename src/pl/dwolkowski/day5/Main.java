9f54b080785688a932544665a8ce9a0ac0b606dd
Dariusz <lavxo@wp.pl>
2021-10-17 15:57:31 +0200

    Add Day5

--- src/pl/dwolkowski/day5/Main.java
+++ src/pl/dwolkowski/day5/Main.java
@@ -2,47 +2,43 @@ package pl.dwolkowski.day5;
 
 import pl.dwolkowski.InputFileReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         InputFileReader file = new InputFileReader("src/pl/dwolkowski/day5/input.txt");
         List<String> list = file.loadStringList();
+        List<Integer> seatIds = new ArrayList<>();
 
+        // Part 1
         for (String line : list) {
-            int index = 0;
-            String row = line.substring(0, 7);
-            String col = line.substring(7, 10);
+            int minRow = 0, maxRow = 127;
+            int minCol = 0, maxCol = 7;
 
-            int min = 1;
-            int mid = 0;
-            int max = 128;
-
-            for (int i = 0; i < row.length(); i++) {
-                mid = (min + max) / 2;
-                if (row.charAt(i) == 'F')
-                    max = mid - 1;
-                else if (row.charAt(i) == 'B')
-                    min = mid + 1;
-
-                System.out.println("Min: " + min + " | Mid: " + mid + " | Max: " + max);
+            for (char c : line.toCharArray()) {
+                switch (c) {
+                    case 'F' -> maxRow = (maxRow - minRow) / 2 + minRow;
+                    case 'B' -> minRow = (maxRow - minRow) / 2 + minRow + 1;
+                    case 'L' -> maxCol = (maxCol - minCol) / 2 + minCol;
+                    case 'R' -> minCol = (maxCol - minCol) / 2 + minCol + 1;
+                }
             }
-            index = mid;
+            seatIds.add((minRow * 8) + minCol);
+        }
+        Collections.sort(seatIds);
+        System.out.println(seatIds.get(seatIds.size()-1));
 
-            min = 1;
-            mid = 0;
-            max = 8;
-
-            for (int i = 0; i < col.length(); i++) {
-                mid = (min + max) / 2;
-                if (row.charAt(i) == 'L')
-                    max = mid - 1;
-                else if (row.charAt(i) == 'B')
-                    min = mid + 1;
+        // Part 2
+        int recentSeat = seatIds.get(0);
+        seatIds.remove(0);
+        for (int id : seatIds) {
+            if (id != (recentSeat + 1)) {
+                System.out.println(recentSeat+1);
+                break;
             }
-            System.out.println(index + " " + mid);
-            break;
-
+            recentSeat = id;
         }
 
     }
