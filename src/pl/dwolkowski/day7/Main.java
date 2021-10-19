0ed7023da8da58d3e712c70e33dab9c54df52b5e
Dariusz <lavxo@wp.pl>
2021-10-19 19:29:59 +0200

    Add Day7

---
+++ src/pl/dwolkowski/day7/Main.java
@@ -0,0 +1,16 @@
+package pl.dwolkowski.day7;
+
+import pl.dwolkowski.InputFileReader;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        InputFileReader file = new InputFileReader("src/pl/dwolkowski/day7/input.txt");
+        List<String> list = file.loadStringList();
+
+
+
+
+    }
+}
